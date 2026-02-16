15a9b930d461242a813904bca47830e0ee998c35
leonkae <leon29kae@gmail.com>
2026-02-16 22:45:29 +0300

    more power

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./global.css";
 import LandingPage from "./components/Landing";
-import {Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import AddProduct from "./components/ProductI";
-
 
 function App() {
   return (
     <div className="app-container">
       <header>
-        <h1>
-          DOLLYS <span>HARD</span>WARE
-        </h1>
+        <div className="inner-header-class">
+          <h1>
+            DOLLYS <span>HARD</span>WARE
+          </h1>
 
-        <nav>
-          <Link to="/add-product" className="btn btn-primary">Add Product</Link>
-        </nav>
+          <nav>
+            <Link to="/add-product" id="btn-style" className="btn btn-primary">
+              Add Product
+            </Link>
+            <Link to="/" id="btn-style" className="btn btn-primary">
+              Home
+            </Link>
+          </nav>
+        </div>
 
         <Routes>
-          <Route path="/add-product" element={<AddProduct />} /> 
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/add-product" element={<AddProduct />} />
         </Routes>
-
       </header>
-      <LandingPage />
     </div>
   );
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,18 +4,12 @@ function LandingPage() {
   return (
     <div className="main-landing-wrapper">
       <div className="inner-landing-page">
-        <div className="landing-title">
-          <h1>Dolly's Hardware</h1>
-          <p>
-            Welcome to Dolly's Hardware, your one-stop shop for all your
-            hardware needs! We offer a wide range of high-quality tools,
-            equipment, and supplies for both professionals and DIY enthusiasts.
-            Whether you're a contractor, a homeowner, or a hobbyist, we have
-            everything you need to get the job done right. Our knowledgeable
-            staff is always ready to assist you in finding the perfect products
-            for your projects. Visit us today and experience the difference at
-            Dolly's Hardware!
-          </p>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="landing-title">
+              <h1>DOLLY'S HARDWARE</h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>
